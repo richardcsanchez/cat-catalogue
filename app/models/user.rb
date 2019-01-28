ed61f5a1d7c0c8281ba07effabd8cf5da0c79090
Richard Sanchez <richard.charles.s@gmail.com>
2019-01-28 16:00:00 -0500

    add adopt_cat method

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,13 @@ class User < ApplicationRecord
   has_many :cats
   has_many :agencies, through: :cats
 
+  def adopt_cat(cat)
+    cat.owner_id = self.id
+    self.money -= cat.cost
+    self.cats << cat
+    self.save
+  end
+
   def self.find_or_create_by_omniauth(auth)
     self.where(email: auth["info"]["email"]).first_or_create do |user|
       user.password = SecureRandom.hex

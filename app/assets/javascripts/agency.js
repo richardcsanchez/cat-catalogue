3e785034b727fc295c6ef02be3b7f67cf8da98cb
Richard Sanchez <richard.charles.s@gmail.com>
2019-03-25 13:12:57 -0400

    stub out agency.js

--- app/assets/javascripts/agency.js
+++ app/assets/javascripts/agency.js
@@ -1,1 +1,19 @@
+//Add New Cat Form - cat.prototype.newCatForm
+//1. browser makes request through click
+//2. appends form to page
+//3.submits form through ajax request
 
+//Add cats index to agency page
+//1.A browser makes request through click (AJAX GET Request)
+  //1.B Renders Cat info in JSON
+//2. appends list of cat names to page
+
+//Render show cat page
+//1. additional click (..see more) adds additional information
+//2. allows user to click to next cat 
+
+
+//Edit Cats:
+//1. Allows user to edit Cats w/in the index page
+//2. prepopulated forms
+//3. allows user to save new information about cat or delete or cancel

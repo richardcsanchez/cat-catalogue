5f3b305cea3439ac68fa1aec858529fa3945f0bd
Richard Sanchez <richard.charles.s@gmail.com>
2019-04-02 16:29:30 -0400

    move js code into file and begin building next button

--- app/assets/javascripts/agency.js
+++ app/assets/javascripts/agency.js
@@ -17,3 +17,72 @@
 //1. Allows user to edit Cats w/in the index page
 //2. prepopulated forms
 //3. allows user to save new information about cat or delete or cancel
+
+function openTab(evt, tab) {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(tab).style.display = "block";
+  evt.currentTarget.className += " active";
+
+}
+
+function moreCatInfo() {
+  document.getElementById("more-cat-info").hidden = false
+};
+
+$(function () {
+  $(".js-next").on("click", function() {
+    var nextId = parseInt($(".js-next").attr("data-id")) + 1;
+    $.getJSON("/agencies/" + nextId + ".json", function(data) {
+      $(".name").text(data["name"]);
+      $(".email").text(data["email"]);
+      $(".phone_number").text(data["phone_number"]);
+      $(".street_1").text(data["street_1"]);
+      $(".street_2").text(data["street_1"]);
+      $(".city").text(data["city"]);
+      $(".state").text(data["state"]);
+      $(".zip_code").text(data["zip_code"]);
+
+      $(".js-next").attr("data-id", data["id"]);
+    });
+  });
+});
+
+$(function () {
+  $(".js-previous").on("click", function() {
+    var previousId = parseInt($(".js-previous").attr("data-id")) + 1;
+    $.getJSON("/agencies/" + previousId + ".json", function(data) {
+      $(".name").text(data["name"]);
+      $(".email").text(data["email"]);
+      $(".phone_number").text(data["phone_number"]);
+      $(".street_1").text(data["street_1"]);
+      $(".street_2").text(data["street_1"]);
+      $(".city").text(data["city"]);
+      $(".state").text(data["state"]);
+      $(".zip_code").text(data["zip_code"]);
+
+      $(".js-previous").attr("data-id", data["id"]);
+    });
+  });
+});
+
+// function editAgency(){
+//
+//   $.ajax({
+//     type: 'GET',
+//     url: '/agencies/',
+//     success: function(data){
+//       console.log("clicked")
+//     }
+//   })
+// }

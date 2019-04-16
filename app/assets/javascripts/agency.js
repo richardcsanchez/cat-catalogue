cba433fb276a3cda4435c84c738d4726ce1e02c1
Richard Sanchez <richard.charles.s@gmail.com>
2019-04-16 12:53:18 -0400

    format js code

--- app/assets/javascripts/agency.js
+++ app/assets/javascripts/agency.js
@@ -1,119 +1,83 @@
-//Add New Cat Form - cat.prototype.newCatForm
-//1. browser makes request through click
-//2. appends form to page
-//3.submits form through ajax request
-
-//Add cats index to agency page
-//1.A browser makes request through click (AJAX GET Request)
-  //1.B Renders Cat info in JSON
-//2. appends list of cat names to page
-
-//Render show cat page
-//1. additional click (..see more) adds additional information
-//2. allows user to click to next cat
+$(document).ready(function() {
+  loadCatIndex()
+  toggleTabs()
+  newCat()
+  newCatFormReload()
+})
 
 
-//Edit Cats:
-//1. Allows user to edit Cats w/in the index page
-//2. prepopulated forms
-//3. allows user to save new information about cat or delete or cancel
+  function loadCatIndex(){
+  $('.newCat').hide()
+  $('.cats-index').hide()
+  let agencyId = parseInt($(".agency-show").attr("data-id"))
+  fetch('/agencies/' + agencyId + '/cats.json')
+    .then(res => res.json())
+    .then(cats => {
+      cats.forEach(cat => {
 
-// function openTab(evt, tab) {
-//   var i, tabcontent, tablinks;
-//
-//   tabcontent = document.getElementsByClassName("tabcontent");
-//   for (i = 0; i < tabcontent.length; i++) {
-//     tabcontent[i].style.display = "none";
-//   }
-//
-//   tablinks = document.getElementsByClassName("tablinks");
-//   for (i = 0; i < tablinks.length; i++) {
-//     tablinks[i].className = tablinks[i].className.replace(" active", "");
-//   }
-//
-//   document.getElementById(tab).style.display = "block";
-//   evt.currentTarget.className += " active";
-//
-// }
-$(document).ready(function(){
-  $("#cat-index-button").on("click", function() {
-    $('.cats-index').toggle()
-    $('.newCat').hide()
-    })
+        let newCat = new Cat(cat)
+        let catHTML = newCat.appendButton()
+        $('.cats-index').append(catHTML)
 
-  $('#new-cat-button').on("click", function() {
-      $('.newCat').toggle()
-      $('.cats-index').hide()
     })
   })
+  }
 
-$(window).load(function() {
-  $('.newCat').hide()
-  let agencyId = parseInt($(".agency-show").attr("data-id"))
-    fetch('/agencies/' + agencyId + '/cats.json')
-      .then(res => res.json())
-      .then(cats => {
-        cats.forEach(cat => {
+  function toggleTabs() {
+    $("#cat-index-button").on("click", function() {
+      $('.cats-index').toggle()
+      $('.newCat').hide()
+      })
 
-          let newCat = new Cat(cat)
-          let catHTML = newCat.appendButton()
-          $('.cats-index').append(catHTML)
+    $('#new-cat-button').on("click", function() {
+        $('.newCat').toggle()
+        $('.cats-index').hide()
+      })
+    }
 
+  function newCat(){
+    $("#new_cat").on("submit", function(e){
+      e.preventDefault()
+      const values = $(this).serialize()
+
+      $.post('/cats', values)
+        .done(function(data) {
+
+          $('#new_cat').trigger("reset")
+
+          $('#new-cat-form').hide()
+
+          const newCat = new Cat(data)
+
+          const htmlToAdd = newCat.catInfo()
+          $("#new-cat-show").show()
+           $("#new-cat-show").html(htmlToAdd)
+
+           const buttonToAdd = newCat.appendButton()
+           $('.cats-index').append(buttonToAdd)
+
+           $('#newCatFormReload').css('display','block');
       })
     })
   }
-)
 
-
-
-
-
-function catShowPage(catId){
-  var agencyId = parseInt($(".agency-id").attr("data-id"))
-  window.location.href = "/agencies/" + agencyId + "/cats/" + catId
+  function newCatFormReload(){
+    $('#newCatFormReload').on("click", function(e){
+      $('#new-cat-form').show()
+      $("#new-cat-show").hide()
+      $('#newCatFormReload').hide()
+    })
   }
 
+  function catShowPage(catId){
+    var agencyId = parseInt($(".agency-id").attr("data-id"))
+    window.location.href = "/agencies/" + agencyId + "/cats/" + catId
+    }
 
-function agencyShowPage(agencyId){
-  window.location.href= "/agencies/" + agencyId
-}
+  function agencyShowPage(agencyId){
+    window.location.href= "/agencies/" + agencyId
+  }
 
-function editAgency(agencyId){
-  window.location.href= "/agencies/" + agencyId + "/edit"
-}
-
-$(document).ready(function(){
-
-  $("#new_cat").on("submit", function(e){
-    e.preventDefault()
-    const values = $(this).serialize()
-
-    $.post('/cats', values)
-      .done(function(data) {
-
-        $('#new_cat').trigger("reset")
-
-        $('#new-cat-form').hide()
-
-        const newCat = new Cat(data)
-
-        const htmlToAdd = newCat.catInfo()
-        $("#new-cat-show").show()
-         $("#new-cat-show").html(htmlToAdd)
-
-         const buttonToAdd = newCat.appendButton()
-         $('.cats-index').append(buttonToAdd)
-
-         $('#newCatFormReload').css('display','block');
-    })
-  })
-})
-
-
-$(document).ready(function(){
-  $('#newCatFormReload').on("click", function(e){
-    $('#new-cat-form').show()
-    $("#new-cat-show").hide()
-    $('#newCatFormReload').hide()
-  })
-})
+  function editAgency(agencyId){
+    window.location.href= "/agencies/" + agencyId + "/edit"
+  }
